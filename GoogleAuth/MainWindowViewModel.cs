de7e7ec289121783bb81cd7ba78d954cf71ebe7e
marwan <marwan@DESKTOP-FGFSOKP>
2024-01-04 23:42:03 +0100

    Add project files.

---
+++ GoogleAuth/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GoogleAuth.Services;
+
+namespace GoogleAuth
+{
+    public partial class  MainWindowViewModel : ObservableRecipient
+    {
+        private const string successed = "#69F0AE";
+        private const string failed = "#FF9E80";
+        [ObservableProperty]
+        private string response;
+        [ObservableProperty]
+        private string background;
+        [ObservableProperty]
+        private string forecolor;
+        private readonly GoogleAuthServices googleAuth = new();
+        [RelayCommand]
+        private void Login()
+        {
+            try
+            {
+                var credential = googleAuth.AuthenticateAsync().Result;
+                Response = "successed";
+                Background = successed;
+                Forecolor = "#1B5E20";
+            }
+            catch (Exception ex)
+            {
+                Background = failed;
+                Response = ex.Message;
+                Forecolor = "#DD2C00";
+            }
+                
+
+        }
+
+    }
+}
---
+++ GoogleAuth/Services/GoogleAuthServices.cs
@@ -0,0 +1,37 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Plus.v1;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleAuth.Services
+{
+    internal class GoogleAuthServices
+    {
+        private const string ClientId = "xxxx";
+        private const string ClientSecret = "xxxx";
+
+        public async Task<UserCredential> AuthenticateAsync(CancellationToken cancellationToken = default)
+        {
+            
+
+            var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = ClientId,
+                    ClientSecret = ClientSecret
+                },
+                new[] { PlusService.Scope.UserinfoProfile },
+                "user",
+                cancellationToken,
+                new FileDataStore("Google.Apis.Auth.WPF.Store")
+            );
+
+            return credential;
+        }
+    }
+}
